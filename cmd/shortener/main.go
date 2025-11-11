984bb2696f23d9e682f6439e83dce549b028a0da
Igor Makarov <IgorMakarovHimself@gmail.com>
2025-11-11 20:37:11 +0300

    Add middleware logger

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"link-shortener/internal/config"
 	"link-shortener/internal/handler"
+	"link-shortener/internal/middleware"
 	"link-shortener/internal/repository"
 	"link-shortener/internal/service"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 var urlStorage = make(map[string]string)
@@ -16,11 +18,20 @@ var urlStorage = make(map[string]string)
 func main() {
 	cfg := config.SetupConfig()
 
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	sugar := logger.Sugar()
+
 	repo := repository.NewLocalRepository()
 	svc := service.NewShortenerService(repo)
 	h := handler.NewHandler(svc, cfg.BaseURL)
 
 	r := chi.NewRouter()
+	r.Use(middleware.WithLogging(sugar))
 	r.Post("/", h.HandlePost)
 	r.Get("/{id}", h.HandleGet)
 
