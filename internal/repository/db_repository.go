402f83b317565525f84780620baebf4799ea4c5f
Igor Makarov <IgorMakarovHimself@gmail.com>
2025-12-11 00:40:28 +0300

    Fix db bootstrap

--- internal/repository/db_repository.go
+++ internal/repository/db_repository.go
@@ -40,7 +40,8 @@ func (r *DBRepository) bootstrap(ctx context.Context) error {
 		id SERIAL PRIMARY KEY,
 		short_url VARCHAR(255) UNIQUE NOT NULL,
 		original_url TEXT UNIQUE NOT NULL,
-		user_id TEXT NOT NULL DEFAULT ''
+		user_id TEXT NOT NULL DEFAULT '',
+		is_deleted BOOLEAN NOT NULL DEFAULT FALSE
 	);
 
 	CREATE INDEX IF NOT EXISTS idx_urls_short_url ON urls(short_url);
