d42a9fe527fd676d06cc0b26da65845c225f84ff
Igor Makarov <IgorMakarovHimself@gmail.com>
2026-03-10 00:15:13 +0300

    Add generic pool

---
+++ pkg/pool/pool.go
@@ -0,0 +1,30 @@
+package pool
+
+import "sync"
+
+type Resettable interface {
+	Reset()
+}
+
+type Pool[T Resettable] struct {
+	p sync.Pool
+}
+
+func New[T Resettable](newFunc func() T) *Pool[T] {
+	return &Pool[T]{
+		p: sync.Pool{
+			New: func() any {
+				return newFunc()
+			},
+		},
+	}
+}
+
+func (p *Pool[T]) Get() T {
+	return p.p.Get().(T)
+}
+
+func (p *Pool[T]) Put(v T) {
+	v.Reset()
+	p.p.Put(v)
+}
